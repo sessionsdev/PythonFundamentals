2164d0cea11a2acb0024d6f3fa1abd71d9fb61e2
Jonathan Sessions <jsesions@Kristens-iMac.local>
2019-07-19 14:25:20 -0400

    Exercise #2 commit

--- 01_python_fundamentals/01_02_seconds_years.py
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -1,5 +1,14 @@
 '''
 
-From the previous example, move your calculation of how many seconds in a year to a python executable script.
+From the previous example, move your calculation of
+how many seconds in a year to a python executable script.
 
 '''
+
+days_in_year = 365
+hours_in_day = 24
+minutes_in_hours = 60
+seconds_in_minutes = 60
+
+seconds_in_year = days_in_year * hours_in_day * minutes_in_hours * seconds_in_minutes
+print(seconds_in_year)
